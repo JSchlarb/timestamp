0b09f86a34609b53d94dc88387d355689748dea3
danielroth <daniel.roth.ext@gec.io>
2024-08-22 10:14:21 +0200

    Integration Tests and CI Services Working

--- dagger/main.go
+++ dagger/main.go
@@ -52,20 +52,44 @@ func (m *Timestamp) Build(
 	return build
 }
 
+func (m *Timestamp) SchemaRegistryService() *dagger.Service {
+	return dag.Container().
+		From("docker.io/bitnami/schema-registry:7.6").
+		WithEnvVariable("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081").
+		WithEnvVariable("SCHEMA_REGISTRY_KAFKA_BROKERS", "PLAINTEXT://kafka:9055").
+		WithServiceBinding("kafka", m.KafkaService()).
+		WithExposedPort(8081).
+		AsService()
+}
+
+func (m *Timestamp) KafkaService() *dagger.Service {
+	return dag.Container().
+		From("docker.io/bitnami/kafka:3.7").
+		WithEnvVariable("KAFKA_ENABLE_KRAFT", "yes").
+		WithEnvVariable("KAFKA_CFG_NODE_ID", "0").
+		WithEnvVariable("KAFKA_CFG_PROCESS_ROLES", "controller,broker").
+		WithEnvVariable("KAFKA_CFG_CONTROLLER_QUORUM_VOTERS", "0@localhost:9093").
+		WithEnvVariable("KAFKA_CFG_LISTENERS", "PLAINTEXT://:9092,CONTROLLER://:9093,EXTERNAL://:9055").
+		WithEnvVariable("KAFKA_CFG_ADVERTISED_LISTENERS", "PLAINTEXT://localhost:9092,EXTERNAL://kafka:9055").
+		WithEnvVariable("KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP", "CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT,PLAINTEXT:PLAINTEXT").
+		WithEnvVariable("KAFKA_CFG_CONTROLLER_LISTENER_NAMES", "CONTROLLER").
+		WithEnvVariable("KAFKA_CFG_INTER_BROKER_LISTENER_NAME", "PLAINTEXT").
+		WithEnvVariable("KAFKA_CFG_AUTO_CREATE_TOPICS_ENABLE", "true").
+		WithEnvVariable("ALLOW_PLAINTEXT_LISTENER", "yes").
+		WithExposedPort(9055).
+		AsService()
+}
+
 func (m *Timestamp) IntegrationTest(ctx context.Context, source *dagger.Directory) *dagger.Container {
 	mavenCache := dag.CacheVolume("maven-cache")
-	svc := dag.
-		Compose().
-		WithFile("./compose.yaml")
 
-	it := svc.
+	return dag.Container().
 		From("maven:3.9-eclipse-temurin-17").
 		WithMountedCache("/root/.m2", mavenCache).
 		WithMountedDirectory("/app", source).
 		WithWorkdir("/app").
-		WithExec([]string{"curl", "http://localhost:8081"})
-
-	return it
+		WithServiceBinding("schema-registry", m.SchemaRegistryService()).
+		WithExec([]string{"curl", "schema-registry:8081/v1/metadata/id"})
 }
 
 func (m *Timestamp) VulnerabilityCheck(
---
+++ producer/src/test/kotlin/com/github/jschlarb/timestamp/TimestampProducerApplicationIntegrationTest.kt
@@ -0,0 +1,13 @@
+package com.github.jschlarb.timestamp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class TimestampProducerApplicationIntegrationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+}

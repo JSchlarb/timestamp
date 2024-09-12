9b3e72d17860619b06d1513909aac7654a97afea
danielroth <daniel.roth.ext@gec.io>
2024-09-12 10:12:57 +0200

    test and build improvements

--- dagger/main.go
+++ dagger/main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"context"
 	"dagger/timestamp/internal/dagger"
+	"fmt"
 )
 
-type Timestamp struct{}
+type Timestamp struct {
+	Sock *dagger.Socket
+}
 
-func (m *Timestamp) Build(
+func New(sock *dagger.Socket) *Timestamp {
+	return &Timestamp{
+		Sock: sock,
+	}
+}
+
+func (m *Timestamp) GradleBuildImage(
 	ctx context.Context,
 	source *dagger.Directory,
-	publish bool,
-	// +optional
-	registryUser *dagger.Secret,
-	// +optional
-	registryPassword *dagger.Secret,
 ) *dagger.Container {
 	// create a cache volume for Gradle downloads
 	gradleCache := dag.CacheVolume("gradle-cache")
@@ -24,43 +28,27 @@ func (m *Timestamp) Build(
 		WithMountedCache("/home/gradle/.gradle", gradleCache).
 		WithMountedDirectory("/app", source).
 		WithWorkdir("/app").
-		WithServiceBinding("dind", m.Dind()).
-		WithEnvVariable("DOCKER_HOST", "tcp://dind:2375")
-
-	if publish {
-		build = build.
-			WithEnvVariable("DOCKER_REGISTRY_URL", "my.repo.url").
-			WithSecretVariable("DOCKER_USERNAME", registryUser).
-			WithSecretVariable("DOCKER_PASSWORD", registryPassword).
-			WithExec([]string{
-				"./gradlew",
-				"clean",
-				"bootBuildImage",
-				"--publishImage",
-			})
-	} else {
-		build = build.
-			WithExec([]string{
-				"./gradlew",
-				"clean",
-				"bootBuildImage",
-			})
-	}
+		WithUnixSocket("/var/run/docker.sock", m.Sock).
+		WithExec([]string{
+			"./gradlew",
+			"clean",
+			"bootBuildImage",
+		})
 
 	return build
 }
 
-func (m *Timestamp) SchemaRegistryService() *dagger.Service {
+func (m *Timestamp) SchemaRegistryService(kafkaService *dagger.Service) *dagger.Service {
 	return dag.Container().
 		From("docker.io/bitnami/schema-registry:7.6").
 		WithEnvVariable("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081").
 		WithEnvVariable("SCHEMA_REGISTRY_KAFKA_BROKERS", "PLAINTEXT://kafka:9055").
-		WithServiceBinding("kafka", m.KafkaService()).
+		WithServiceBinding("kafka", kafkaService).
 		WithExposedPort(8081).
 		AsService()
 }
 
-func (m *Timestamp) KafkaService() *dagger.Service {
+func (m *Timestamp) KafkaService(serviceName string) *dagger.Service {
 	return dag.Container().
 		From("docker.io/bitnami/kafka:3.7").
 		WithEnvVariable("KAFKA_ENABLE_KRAFT", "yes").
@@ -68,7 +56,7 @@ func (m *Timestamp) KafkaService() *dagger.Service {
 		WithEnvVariable("KAFKA_CFG_PROCESS_ROLES", "controller,broker").
 		WithEnvVariable("KAFKA_CFG_CONTROLLER_QUORUM_VOTERS", "0@localhost:9093").
 		WithEnvVariable("KAFKA_CFG_LISTENERS", "PLAINTEXT://:9092,CONTROLLER://:9093,EXTERNAL://:9055").
-		WithEnvVariable("KAFKA_CFG_ADVERTISED_LISTENERS", "PLAINTEXT://localhost:9092,EXTERNAL://kafka:9055").
+		WithEnvVariable("KAFKA_CFG_ADVERTISED_LISTENERS", fmt.Sprintf("PLAINTEXT://localhost:9092,EXTERNAL://%s:9055", serviceName)).
 		WithEnvVariable("KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP", "CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT,PLAINTEXT:PLAINTEXT").
 		WithEnvVariable("KAFKA_CFG_CONTROLLER_LISTENER_NAMES", "CONTROLLER").
 		WithEnvVariable("KAFKA_CFG_INTER_BROKER_LISTENER_NAME", "PLAINTEXT").
@@ -96,17 +84,28 @@ func (m *Timestamp) GradleTest(
 	return build
 }
 
+func (m *Timestamp) GradleUnitTest(ctx context.Context, source *dagger.Directory) *dagger.Container {
+	return dag.Container().
+		From("gradle:8.10.0-jdk21").
+		WithMountedDirectory("/app", source).
+		WithWorkdir("/app").
+		WithExec([]string{"./gradlew", "test"})
+}
+
 func (m *Timestamp) GradleIntegrationTest(ctx context.Context, source *dagger.Directory) *dagger.Container {
+	kafkaServicename := "kafka"
+	kafkaService := m.KafkaService(kafkaServicename)
 
 	return dag.Container().
 		From("gradle:8.10.0-jdk21").
 		WithMountedDirectory("/app", source).
 		WithWorkdir("/app").
-		WithServiceBinding("schema-registry", m.SchemaRegistryService()).
-		WithExec([]string{"./gradlew", "schema-registry:8081/v1/metadata/id"})
+		WithServiceBinding(kafkaServicename, kafkaService).
+		WithServiceBinding("schema-registry", m.SchemaRegistryService(kafkaService)).
+		WithExec([]string{"./gradlew", "integrationTest"})
 }
 
-func (m *Timestamp) VulnerabilityCheck(
+func (m *Timestamp) VulnerabilityCheckFS(
 	source *dagger.Directory,
 	// +optional
 	severity string,
@@ -124,6 +123,25 @@ func (m *Timestamp) VulnerabilityCheck(
 		WithMountedDirectory("/app", source).
 		WithWorkdir("/app").
 		WithExec([]string{"trivy", "fs", "--severity", severity, "--exit-code", exitCode, "."})
+}
+
+func (m *Timestamp) VulnerabilityCheckImage(
+	image string,
+	// +optional
+	severity string,
+	// +optional
+	exitCode string,
+) *dagger.Container {
+	if severity == "" {
+		severity = "HIGH,CRITICAL"
+	}
+	if exitCode == "" {
+		exitCode = "1"
+	}
+	return dag.Container().
+		From("aquasec/trivy:0.53.0").
+		WithUnixSocket("/var/run/docker.sock", m.Sock).
+		WithExec([]string{"trivy", "image", "--severity", severity, "--exit-code", exitCode, image})
 }
 
 func (m *Timestamp) Dind() *dagger.Service {
--- producer/src/test/kotlin/com/github/jschlarb/timestamp/TimestampProducerApplicationIntegrationTest.kt
+++ producer/src/test/kotlin/com/github/jschlarb/timestamp/TimestampProducerApplicationIntegrationTest.kt
@@ -1,11 +1,16 @@
 package com.github.jschlarb.timestamp
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class TimestampProducerApplicationIntegrationTest {
+    @Autowired
+    private lateinit var producer: TimestampKafkaProducer
+
     @Test
     fun contextLoads() {
+        producer.sendTimestamp()
     }
 }
